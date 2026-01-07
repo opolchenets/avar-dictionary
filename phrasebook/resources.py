0b1283458501220476ac90cb20f531bc5692bbe9
Sayfu Malikov <10.6x83@mail.ru>
2026-01-07 20:22:02 +0300

    some fixes

--- phrasebook/resources.py
+++ phrasebook/resources.py
@@ -18,8 +18,24 @@ class PhrasebookPhraseResource(resources.ModelResource):
         # Удаляем section_id, если он есть
         return [f for f in fields if f.attribute != 'section_id']
     
+    def get_or_init_instance(self, instance_loader, row):
+        """Используем section и text для поиска существующей записи."""
+        try:
+            section_name = row.get('section', '')
+            text = row.get('text', '')
+            
+            if section_name and text:
+                section = PhrasebookSection.objects.get(name=section_name)
+                instance = PhrasebookPhrase.objects.get(section=section, text=text)
+                return instance, False
+        except (PhrasebookSection.DoesNotExist, PhrasebookPhrase.DoesNotExist):
+            pass
+        
+        return super().get_or_init_instance(instance_loader, row)
+    
     class Meta:
         model = PhrasebookPhrase
         fields = ('id', 'section', 'text', 'translation', 'translit')
         exclude = ('section_id',)
         import_id_fields = []
+        skip_unchanged = True
